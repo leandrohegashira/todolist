6bdcfe2f802eb4a170f3195ed66d8b67159f8348
Leandro <leandrohegashira>
2022-08-23 17:51:58 -0300

    Finalizando projeto

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+
 // Seleção de Elementos
 const todoForm = document.querySelector("#todo-form")
 const todoInput = document.querySelector("#todo-input")
@@ -6,6 +7,7 @@ const editForm = document.querySelector("#edit-form")
 const editInput = document.querySelector("#edit-input")
 const cancelEditBtn = document.querySelector("#cancel-edit-btn")
 
+let oldInputValue;
 
 // Funções
 const saveTodo = (text) => {
@@ -16,10 +18,7 @@ const saveTodo = (text) => {
     const todoTitle = document.createElement("h3")
     todoTitle.innerText = text
     todo.appendChild(todoTitle)
-
-    console.log(todo)
-
-    /*
+    
     const doneBtn = document.createElement("button")
     doneBtn.classList.add("finish-todo")
     doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>'
@@ -35,16 +34,89 @@ const saveTodo = (text) => {
     deleteBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>'
     todo.appendChild(deleteBtn)
 
-    todoList.appendChild(todo)*/
+    todoList.appendChild(todo)
+
+    todoInput.value = ""
+    todoInput.focus()
+}
+
+const toggleForms = () => {
+    editForm.classList.toggle("hide")
+    todoForm.classList.toggle("hide")
+    todoList.classList.toggle("hide")
+
+}
+
+const updateTodo = (text) => {
+
+    const todos = document.querySelectorAll(".todo")
+
+    todos.forEach((todo) => {
+
+        let todoTitle = todo.querySelector("h3")
+
+        if(todoTitle.innerText === oldInputValue){
+            todoTitle.innerText = text
+        }
+
+    })
+
 }
 
 // Eventos
 todoForm.addEventListener("submit", (e) => {
-    e.preventDefault();
+    e.preventDefault()
 
-    const inputValue = todoInput.value;
+    const inputValue = todoInput.value
 
     if(inputValue){
         saveTodo(inputValue)
     }
 })
+
+
+document.addEventListener("click", (e) => {
+    const targetEl = e.target
+    const parentEl = targetEl.closest("div") //selecionei o elemento div pai mais próximo
+    let todoTitle
+
+    if(parentEl && parentEl.querySelector("h3")){
+        todoTitle = parentEl.querySelector("h3").innerText }
+
+    if(targetEl.classList.contains("finish-todo")){
+        parentEl.classList.toggle("done")
+    }
+
+    if(targetEl.classList.contains("remove-todo")){
+        parentEl.remove()
+    }
+
+    if(targetEl.classList.contains("edit-todo")){
+        toggleForms()
+
+        editInput.value = todoTitle
+        oldInputValue = todoTitle
+    }
+    
+})
+
+cancelEditBtn.addEventListener("click", (e) => {
+    e.preventDefault()
+    toggleForms()
+})
+
+editForm.addEventListener("submit", (e) => {
+
+    e.preventDefault()
+
+    const editInputValue = editInput.value
+
+    if(editInputValue){
+        //atualizar
+        updateTodo(editInputValue)
+    }
+
+    toggleForms()
+})
+
+
